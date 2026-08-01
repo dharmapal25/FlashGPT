27780a94e1c4899187296a36ce6b7f529c79e5b7
dharmapalbharati-web <dharmapalbharati2525@gmail.com>
2026-08-01 21:47:50 +0530

    feat: refactor chat and user models; remove unused test route; update CORS configuration

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import env from "./config/env.js";
 
 import { chatRoute } from "./routes/chat.route.js";
-import router from "./routes/test.route.js";
 
 
 const app = express();
@@ -11,12 +10,11 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: env.FRONTEND_URL,
         credentials: true,
     })
 );
 
-app.use("/api/test", router);
 
 app.use('/api', chatRoute)
 

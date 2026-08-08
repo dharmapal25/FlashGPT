9af38382b2aed2a95f3f282f4cef83e5b7be9b61
dharmapalbharati-web <dharmapalbharati2525@gmail.com>
2026-08-08 20:25:46 +0530

    fix: ensure maxAge is set for cookie in googleCallback function

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -21,6 +21,7 @@ export const googleCallback = (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
   const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
